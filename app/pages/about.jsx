3112eec323cd5a4a7625a5aa7793ddd88be69ec3
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-02-19 13:33:32 -0300

    Update about.jsx

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Text } from "react-native";
+import { Linking, Text } from "react-native";
 import Page from "../components/page";
 
 export default function About() {
 
   return (
     <Page>
-      <Text>This app demonstrates how to implement OAuth2 authentication and communicate with backend.</Text>
+      <Text style={{ fontSize: 20 }}>This app demonstrates how to implement OAuth2 authentication and communicate with backend.</Text>
+      <Text />
+      <Text>Developed by Digao Dalpiaz</Text>
+      <Text onPress={gotoSite} style={{ color: 'blue', fontSize: 20 }}>GitHub</Text>
     </Page>
   )
+
+  function gotoSite() {
+    Linking.openURL('https://github.com/digao-dalpiaz');
+  }
 
 }
